1cd870853d0687b0f2a8fefb58d2dc7955b8b12b
capxax <20886982+capxax@users.noreply.github.com>
2022-06-12 20:00:14 +0800

    Update jkm.js

--- jkm.js
+++ jkm.js
@@ -1,10 +1,1 @@
-if ($request.url.indexOf('hbjkm/flts/hstc.json') != -1) {
-    if($response.status!=200){
-        
-        $done({status: 200, headers: $response.headers, body: '{"tcts":0,"tsxx":"提示","wxtx":"疫情防控"}' })
-    }else{
-        $done({})
-    }
-}else{
-    $done({})
-}
+$done({status: 200, headers: $response.headers, body: '{"tcts":0,"tsxx":"提示","wxtx":"疫情防控"}' })
