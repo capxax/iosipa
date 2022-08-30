b231e229f0f7670e07b1a4d701cfc1c3bd5aff3b
capxax <20886982+capxax@users.noreply.github.com>
2022-08-30 23:09:10 +0800

    Update bjtc.js

--- bjtc.js
+++ bjtc.js
@@ -1,1 +1,1 @@
-$done({status: 200, headers: $response.headers, body: '{"errcode":"BIS:10200","errmsg":null,"data":{"code":"0"}}' })
+$done({status: 200, headers: $response.headers, body: '{"errcode":"BIS:10200","errmsg":null,"data":{"code":"Ivl7cBko+tfg2m7bk2C7Ww=="}}' })
